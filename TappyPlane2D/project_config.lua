f90d4401d94920830d8c35af877bbbde19f8d829
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-08 18:27:08 +0900

    TappyPlane2D 프로젝트를 설정하는 premake5 스크립트 추가

---
+++ TappyPlane2D/project_config.lua
@@ -0,0 +1,44 @@
+project (game_project) -- 게임 프로젝트입니다.
+        kind "ConsoleApp" -- 프로젝트의 종류를 설정합니다.
+        language "C++" -- 프로젝트의 프로그래밍 언어를 설정합니다.
+        cppdialect "C++17" -- C++의 표준을 설정합니다.
+
+        flags { 
+            "MultiProcessorCompile", -- 멀티 스레드 빌드를 활성화합니다.
+        }
+
+        includedirs {
+            "%{game_directory}/Source", -- 게임 소스 파일 경로를 추가합니다.
+            
+            "%{engine_directory}/Source", -- 엔진의 소스 파일 경로를 추가합니다.
+        }
+    
+        files {
+            "%{game_directory}/**", -- 게임 파일 경로를 추가합니다.
+        }
+
+        links {
+            "GameEngine", -- 게임 엔진 라이브러리를 추가합니다.
+        }
+
+        disablewarnings { 
+            "4996", -- C4996 경로를 비활성화합니다.
+        }
+
+        filter "configurations:Debug" -- 프로젝트의 Debug 모드 설정을 수행합니다.
+            defines { "DEBUG" }
+            runtime  "Debug"
+            optimize "Off"
+            symbols "On"
+
+        filter "configurations:Release" -- 프로젝트의 Release 설정을 수행합니다.
+            defines { "NDEBUG", "RELEASE" }
+            runtime "Release"
+            optimize "On"
+            symbols "On"
+
+        filter "configurations:Shipping" -- 프로젝트의 Shipping 설정을 수행합니다.
+            defines { "NDEBUG", "SHIPPING" }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
