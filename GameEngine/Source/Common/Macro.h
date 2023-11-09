49e4a23e8c523ec1ac4beeaf8f37c9bc5dd9d35d
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-09 15:53:34 +0900

    타입을 싱글턴으로 설정해는 매크로 추가

--- GameEngine/Source/Common/Macro.h
+++ GameEngine/Source/Common/Macro.h
@@ -25,3 +25,25 @@ TypeName& operator=(const TypeName) = delete;
 TypeName() = default;\
 virtual ~TypeName() {}
 #endif
+
+/**
+ * @brief 타입(클래스 혹은 구조체)을 싱글턴으로 설정합니다.
+ *
+ * @param TypeName 싱글턴으로 설정할 타입 이름입니다.
+ */
+#ifndef SINGLETON
+#define SINGLETON(TypeName)\
+public:\
+TypeName(TypeName&&) = delete; \
+TypeName(const TypeName&) = delete; \
+TypeName& operator=(TypeName&&) = delete; \
+TypeName& operator=(const TypeName) = delete;\
+static TypeName& Get()\
+{\
+static TypeName instance; \
+return instance; \
+}\
+private:\
+TypeName() = default;\
+virtual ~TypeName() {}
+#endif
