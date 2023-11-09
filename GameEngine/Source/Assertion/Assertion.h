714f01fd45dab748e3a72af1228bab8607b98afb
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-10 07:37:43 +0900

    디버그 창에 형식화된 문자열을 출력하는 기능 추가

---
+++ GameEngine/Source/Assertion/Assertion.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdarg>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <windows.h>
+
+
+/**
+ * @brief 디버그 창에 형식화된 문자열을 출력합니다.
+ * 
+ * @param format 형식 문자열입니다.
+ * @param .. 문자열 내의 형식에 대응하는 가변인자입니다.
+ * 
+ * @note
+ * - Debug와 Release모드에서만 디버그 창에 문자열이 출력됩니다.
+ * - Shipping 모드에서는 디버그 창에 문자열이 출력되지 않습니다.
+ */
+inline void DebugPrintF(const char* format, ...)
+{
+	const int32_t BUFFER_SIZE = 1024;
+	char buffer[BUFFER_SIZE];
+	std::memset(buffer, 0, sizeof(buffer));
+
+	va_list args;
+	va_start(args, format);
+	int32_t size = vsnprintf(buffer, BUFFER_SIZE, format, args);
+	va_end(args);
+
+	OutputDebugStringA(buffer);
+}
