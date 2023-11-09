959684b3549d8a5db065551c55f93af08a1551c3
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-09 11:42:22 +0900

    Visual Studio 솔루션 및 하위 프로젝트를 업데이트하는 배치 스크립트 추가

---
+++ GameEngine/Script/HotReload.bat
@@ -0,0 +1,36 @@
+@echo off
+
+@REM 게임 엔진 경로입니다.
+SET engine_path=%~dp0..\
+
+@REM Premake5 실행 파일 경로입니다.
+SET premake5_path=%engine_path%Bin\premake5.exe
+
+@REM Visual Studio 버전입니다. 
+SET visualstudio=%1
+
+@REM 프로젝트 이름입니다.
+SET project=%2
+
+echo START GenerateProjectFiles.bat...
+
+@REM visual studio 버전을 검사합니다. 2019 혹은 2022입니다.
+if "%visualstudio%" == "vs2019" (
+    echo Visual Studio version is %visualstudio%...
+) else if "%visualstudio%" == "vs2022" (
+    echo Visual Studio version is %visualstudio%...
+) else (
+    echo Visual Studio version %visualstudio% is illegal...
+    GOTO:EOF
+)
+
+@REM 프로젝트 이름의 유효성을 검사합니다.
+if "%project%" == "" (
+    echo project name is empty...
+    GOTO:EOF
+) else (
+    echo Project name is %project%
+)
+
+echo START premake5.exe...
+%premake5_path% %visualstudio% %project%
