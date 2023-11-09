bc66ab267fc6f3b28f37a6743b485d71475443b3
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-10 08:03:25 +0900

    평가식을 검사하는 어선셜 매크로 추가

--- GameEngine/Source/Assertion/Assertion.h
+++ GameEngine/Source/Assertion/Assertion.h
@@ -31,6 +31,7 @@ inline void DebugPrintF(const char* format, ...)
 	OutputDebugStringA(buffer);
 }
 
+
 /**
  * @brief 디버그 창에 형식화된 문자열을 출력합니다.
  * 
@@ -54,3 +55,34 @@ inline void DebugPrintF(const wchar_t* format, ...)
 
 	OutputDebugStringW(buffer);
 }
+
+
+/**
+ * @brief 평가식을 검사합니다.
+ * 
+ * @param EXPRESSION 검사할 평가식입니다.
+ * @param ... 평가식을 만족하지 못할 경우 표시할 가변 인자 메시지입니다.
+ * 
+ * @note
+ * - Debug 모드와 Release 모드에서는 평가식을 검사하지만 Shipping 모드에서는 평가식을 검사하지 않습니다.
+ * - 디버거가 존재하면 브레이크 포인트가 걸립니다.
+ * - 디버거가 존재하지 않으면 크래시 덤프 파일을 생성합니다.
+ */
+#if defined(DEBUG) || defined(RELEASE)
+#ifndef ASSERTION
+#define ASSERTION(EXPRESSION, ...)\
+{\
+if (!(bool)(EXPRESSION))\
+{\
+DebugPrintF("\nAssertion check point failed!\nFILE : %s\nLINE : %d\nEXPRESSION : %s\nMESSAGE : ", __FILE__, __LINE__, #EXPRESSION);\
+DebugPrintF(__VA_ARGS__);\
+DebugPrintF("\n");\
+__debugbreak(); \
+}\
+}
+#endif
+#else
+#ifndef ASSERTION
+#define ASSERTION(EXPRESSION, ...) ((void)(EXPRESSION))
+#endif
+#endif
