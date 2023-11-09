0ebc432a8e5a1400aa0412b2d37660248b3ea0dd
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-09 11:53:39 +0900

    Visual Studio 프로젝트 모드에 맞는 빌드 스크립트 추가

---
+++ Build_Release.bat
@@ -0,0 +1,7 @@
+@echo off
+
+SET project=TappyPlane2D
+SET mode=Release
+SET script=GameEngine\Script\BuildSolution.bat
+
+%script% %project% %mode%
---
+++ Build_Shipping.bat
@@ -0,0 +1,7 @@
+@echo off
+
+SET project=TappyPlane2D
+SET mode=Shipping
+SET script=GameEngine\Script\BuildSolution.bat
+
+%script% %project% %mode%
