839eb7986d42d30627003ea8022975681dca7b30
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-09 16:10:34 +0900

    표준 문자열(std::string, std::wstring)의 확장 기능을 제공하는 클래스 선언 추가
    - 클래스의 모든 멤버 변수와 메서드 모두 정적(static)이 되도록 구현 예정

---
+++ GameEngine/Source/Common/StringUtils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <cstdarg>
+
+
+/**
+ * @brief 표준 문자열(std::string, std::wstring)의 확장 기능을 제공합니다.
+ *
+ * @note 이 클래스의 멤버 변수 및 멤버 함수는 모두 정적(static)입니다.
+ */
+class StringUtils
+{
+public:
+};
