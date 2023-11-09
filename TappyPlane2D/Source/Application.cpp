a887b6d4c03b4d2f5a12a305bc6fdf3cf7ca6b54
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-09 17:17:28 +0900

    GLFW를 초기화하고 윈도우 창을 생성 및 백버퍼 초기화 기능 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,6 +1,61 @@
-#include <iostream>
+#include <string>
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
 
 int main(int argc, char* argv[])
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	const std::string& title = "TappyPlane2D";
+	const int32_t width = 1000;
+	const int32_t height = 800;
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, 
+		[](GLFWwindow* window, int width, int height)
+		{
+			glViewport(0, 0, width, height);
+		}
+	);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		glfwDestroyWindow(window);
+		window = nullptr;
+
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, true);
+		}
+
+		glClearColor(0.1f, 0.5f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	window = nullptr;
+
+	glfwTerminate();
 	return 0;
 }
