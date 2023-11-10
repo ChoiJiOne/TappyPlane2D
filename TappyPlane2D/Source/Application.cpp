0fab8cc5a320282323dbb2345f4c9c3fb4fea347
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-11 08:32:44 +0900

    명령행 인자 파싱 테스트 코드 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,6 +1,19 @@
 #include "AssertionMacro.h"
+#include "CommandLineArg.h"
+#include "StringUtils.h"
 
 int main(int argc, char* argv[])
 {
+	CommandLineArg::Parse(argc, argv);
+
+	int32_t value0;
+	CommandLineArg::GetIntValue("C", value0);
+
+	float value1;
+	CommandLineArg::GetFloatValue("B", value1);
+
+	std::string value2;
+	CommandLineArg::GetStringValue("C", value2);
+
 	return 0;
 }
