85297db92e68b94920e492bf46f194b5be9f1fed
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-11 19:04:20 +0900

    파일 시스템 삭제 및 파일 매니저 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,18 +1,15 @@
 #include "AssertionMacro.h"
 #include "CommandLineArg.h"
-#include "FileSystem.h"
+#include "FileManager.h"
 #include "StringUtils.h"
+
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
 	CommandLineArg::Parse(argc, argv);
 
-	std::wstring dir = L"D:\\Work\\TappyPlane2D\\TappyPlane2D";
-	std::vector<std::wstring> contents0 = FileSystem::GetSubDirectories(dir);
-	std::vector<std::wstring> contents1 = FileSystem::GetFilesInDirectory(dir);
-	std::vector<std::wstring> contents2 = FileSystem::GetDirectoryContents(dir);
-
-	std::wstring text = FileSystem::ReadTextFileToString(L"D:\\Work\\TappyPlane2D\\TappyPlane2D\\korean.txt");
-
+	FileManager::Get().Startup();
+	FileManager::Get().Shutdown();
 	return 0;
 }
