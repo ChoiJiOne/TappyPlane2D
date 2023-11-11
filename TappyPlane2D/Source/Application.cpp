5c1b57c2d8ab3d0c7a1e51bee81bd3b960a9b4b4
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-11 17:33:44 +0900

    파일 시스템 기능 구현을 위한 테스트 코드 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,10 +1,18 @@
 #include "AssertionMacro.h"
 #include "CommandLineArg.h"
+#include "FileSystem.h"
 #include "StringUtils.h"
 
 int main(int argc, char* argv[])
 {
 	CommandLineArg::Parse(argc, argv);
+
+	std::wstring dir = L"D:\\Work\\TappyPlane2D\\TappyPlane2D";
+	std::vector<std::wstring> contents0 = FileSystem::GetSubDirectories(dir);
+	std::vector<std::wstring> contents1 = FileSystem::GetFilesInDirectory(dir);
+	std::vector<std::wstring> contents2 = FileSystem::GetDirectoryContents(dir);
+
+	std::wstring text = FileSystem::ReadTextFileToString(L"D:\\Work\\TappyPlane2D\\TappyPlane2D\\korean.txt");
 
 	return 0;
 }
