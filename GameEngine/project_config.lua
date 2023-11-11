679b16a330e96a6ceb7d07c29d86e4eb40f0281a
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-11 12:11:58 +0900

    필요 없어진 Windows 라이브러리 삭제

--- GameEngine/project_config.lua
+++ GameEngine/project_config.lua
@@ -26,7 +26,6 @@ project "GameEngine"    -- 게임 엔진 프로젝트입니다.
 
     links {
         "Dbghelp.lib", -- 디버깅을 위한 라이브러리를 추가합니다.
-        "Shlwapi.lib",
         "glad",
         "glfw",
         "imgui",
