e5ad2af1ade1edccedb150b23f087a136a4d3c07
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-11 13:19:26 +0900

    테스트용 코드 모두 삭제

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -6,14 +6,5 @@ int main(int argc, char* argv[])
 {
 	CommandLineArg::Parse(argc, argv);
 
-	int32_t value0;
-	CommandLineArg::GetIntValue("C", value0);
-
-	float value1;
-	CommandLineArg::GetFloatValue("B", value1);
-
-	std::string value2;
-	CommandLineArg::GetStringValue("C", value2);
-
 	return 0;
 }
