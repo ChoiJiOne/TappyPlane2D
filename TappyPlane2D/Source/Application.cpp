4e0e51197f83e5895057ffc672e907f09f0ab5ce
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-11 19:53:18 +0900

    모든 초기화 관련 코드 삭제
    - 모든 초기화는 엔진 매니저에서 진행할 예정

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,21 +1,13 @@
 #include "AssertionMacro.h"
 #include "CommandLineArg.h"
+#include "EngineManager.h"
 #include "FileManager.h"
 #include "StringUtils.h"
 
-#include <iostream>
-
 int main(int argc, char* argv[])
 {
-	CommandLineArg::Parse(argc, argv);
+	EngineManager::Get().Startup();
+	EngineManager::Get().Shutdown();
 
-	FileManager::Get().Startup();
-
-	std::string propertiesPath;
-	ASSERT(CommandLineArg::GetStringValue("properties", propertiesPath), "failed to load properties json file...");
-
-	Json::Value properties = FileManager::Get().ReadJsonFile(propertiesPath);
-
-	FileManager::Get().Shutdown();
 	return 0;
 }
