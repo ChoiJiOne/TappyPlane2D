cf0715f43ccd622088d666c5e7778f89d7018095
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-12 09:26:25 +0900

    아주 간단한 동작을 수행하는 게임 루프 추가
    - 백버퍼를 검정색으로 초기화
    - 뷰포트 설정 부분은 임시로 하드 코딩

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,12 +1,28 @@
-#include "AssertionMacro.h"
-#include "CommandLineArg.h"
 #include "EngineManager.h"
-#include "FileManager.h"
-#include "StringUtils.h"
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
 
 int main(int argc, char* argv[])
 {
 	EngineManager::Get().Startup();
+	
+	GLFWwindow* window = EngineManager::Get().GetWindowPtr()->GetWindowPtr();
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, true);
+		}
+
+		RenderManager::Get().SetViewport(0, 0, 1000, 800);
+		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+		RenderManager::Get().EndFrame();
+
+		glfwPollEvents();
+
+	}
+
 	EngineManager::Get().Shutdown();
 
 	return 0;
