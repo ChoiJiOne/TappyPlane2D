c9082e73aac428edb7c0eb62097689f62265a219
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-14 08:57:01 +0900

    와이어 프레임 사각형을 그리는 기능 추가

--- GameEngine/Include/GeometryShader2D.h
+++ GameEngine/Include/GeometryShader2D.h
@@ -215,6 +215,44 @@ public:
 		const Vector4f& color
 	);
 
+
+	/**
+	 * @brief 화면에 2D 와이어 프레임 직사각형을 그립니다.
+	 *
+	 * @param ortho 직교 투영 행렬입니다.
+	 * @param center 직사각형의 중심 좌표입니다.
+	 * @param width 직사각형의 가로 크기입니다.
+	 * @param heigt 직사각형의 세로 크기입니다.
+	 * @param rotate 직사각형의 라디안 회전 각도입니다.
+	 * @param color 직사각형의 색상입니다.
+	 */
+	void DrawWireframeRectangle2D(
+		const Matrix4x4f& ortho,
+		const Vector2f& center,
+		float width,
+		float height,
+		float rotate,
+		const Vector4f& color
+	);
+
+
+	/**
+	 * @brief 화면에 2D 와이어 프레임 직사각형을 그립니다.
+	 *
+	 * @param ortho 직교 투영 행렬입니다.
+	 * @param leftTopPosition 직사각형의 왼쪽 상단 좌표입니다.
+	 * @param rightBottomPosition 직사각형의 오른쪽 하단 좌표입니다.
+	 * @param rotate 직사각형의 라디안 회전 각도입니다.
+	 * @param color 직사각형의 색상입니다.
+	 */
+	void DrawWireframeRectangle2D(
+		const Matrix4x4f& ortho,
+		const Vector2f& leftTopPosition,
+		const Vector2f& rightBottomPosition,
+		float rotate,
+		const Vector4f& color
+	);
+
 	
 private:
 	/**
