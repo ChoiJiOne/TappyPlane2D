9637552750750dcfbcb12e15bc77673ec96281d5
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-13 17:35:18 +0900

    10000개의 임의의 점 생성 후 렌더링하는 테스트 코드 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -3,45 +3,28 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
-
-//#include <DirectXMath.h>
-#include <iostream>
-
 int main(int argc, char* argv[])
 {
 	EngineManager::Get().Startup();
+	RenderManager::Get().SetDepthMode(false);
 	
 	GLFWwindow* window = EngineManager::Get().GetWindowPtr()->GetWindowPtr();
 
 	std::string shaderPath;
 	CommandLineArg::GetStringValue("glsl", shaderPath);
 
-	Shader* shader = ResourceManager::Get().CreateResource<Shader>("Shader");
+	GeometryShader2D* shader = ResourceManager::Get().CreateResource<GeometryShader2D>("Shader");
 	shader->Initialize(shaderPath + "Geometry2D.vert", shaderPath + "Geometry2D.frag");
 
-	std::vector<Vector3f> vertices = {
-		Vector3f(10.0f, 10.0f, +0.0f), // left  
-		Vector3f(300.0f, 200.0f, +0.0f), // right 
-		Vector3f(500.0f, 50.0f, +0.0f)  // top   
-	};
-
-	unsigned int VAO;
-	unsigned int VBO;
-
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vector3f) * static_cast<uint32_t>(vertices.size()), reinterpret_cast<const void*>(vertices.data()), GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0));
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	
-	RenderManager::Get().SetDepthMode(false);
+	std::vector<Vector3f> positions;
+	for (int32_t count = 0; count < 10000; ++count)
+	{
+		positions.push_back(Vector3f(
+			MathUtils::GenerateRandomFloat(0.0f, 1000.0f),
+			MathUtils::GenerateRandomFloat(0.0f, 800.0f),
+			0.0f
+		));
+	}
 	
 	while (!glfwWindowShouldClose(window))
 	{
@@ -55,26 +38,10 @@ int main(int argc, char* argv[])
 		RenderManager::Get().SetViewport(0, 0, 1000, 800);
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-		shader->Bind();
-		{
-			float time = static_cast<float>(glfwGetTime());
-
-			Matrix4x4f a = MathUtils::CreateTranslation(200.0f, 200.0f, 0.0f);
-			Matrix4x4f b = MathUtils::CreateScale(Vector3f(0.5f));
-			Matrix4x4f c = MathUtils::CreateRotateZ(time);
-			Matrix4x4f m = a * MathUtils::CreateOrtho(0.0f, 1000.0f, 800.0f, 0.0f, -1.0f, 1.0f);
-			
-			shader->SetMatrix4x4fParameter("transform", m);
-
-			Vector4f color(0.0f, 1.0f, 1.0f, 1.0f);
-			shader->SetVector4fParameter("color", color);
-
-			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, static_cast<uint32_t>(vertices.size()));
-		}
-		shader->Unbind();
-
-
+		Matrix4x4f ortho = MathUtils::CreateOrtho(0.0f, 1000.0f, 800.0f, 0.0f, -1.0f, 1.0f);
+		shader->DrawLine2D(ortho, Vector3f(0.0f, 0.0f, 0.0f), Vector3f(300.0f, 150.0f, 0.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
+		//shader->DrawPoints2D(ortho, positions, Vector4f(0.0f, 1.0f, 0.0f, 1.0f), 3.0f);
+		
 		RenderManager::Get().EndFrame();
 	}
 
