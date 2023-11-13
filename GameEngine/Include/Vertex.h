77a9012ba7262257c36cbc531048eae2b54fc21a
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-13 16:14:17 +0900

    위치와 색상 정보를 가진 정점 추가

--- GameEngine/Include/Vertex.h
+++ GameEngine/Include/Vertex.h
@@ -114,3 +114,137 @@ struct VertexPosition
 	 */
 	Vector3f position_;
 };
+
+
+/**
+ * @brief 위치와 색상 정보를 가진 정점(Vertex)입니다.
+ */
+struct VertexPositionColor
+{
+	/**
+	 * @brief 위치 정보를 가진 정점의 기본 생성자입니다.
+	 */
+	VertexPositionColor() noexcept
+		: position_(0.0f, 0.0f, 0.0f)
+		, color_(0.0f, 0.0f, 0.0f, 0.0f) {}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param position 정점의 위치입니다.
+	 * @param color 정점의 색상입니다.
+	 */
+	VertexPositionColor(Vector3f&& position, Vector4f&& color) noexcept
+		: position_(position) 
+		, color_(color) {}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param position 정점의 위치입니다.
+	 * @param color 정점의 색상입니다.
+	 */
+	VertexPositionColor(const Vector3f& position, const Vector4f& color) noexcept
+		: position_(position) 
+		, color_(color) {}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param x 위치의 X좌표입니다.
+	 * @param y 위치의 Y좌표입니다.
+	 * @param z 위치의 Z좌표입니다.
+	 * @param r 정점 색상의 R입니다.
+	 * @param g 정점 색상의 G입니다.
+	 * @param b 정점 색상의 B입니다.
+	 * @param a 정점 색상의 A입니다.
+	 */
+	VertexPositionColor(
+		float x, float y, float z,
+		float r, float g, float b, float a
+	) noexcept
+		: position_(x, y, z) 
+		, color_(r, g, b, a) {}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionColor(VertexPositionColor&& instance) noexcept
+		: position_(instance.position_) 
+		, color_(instance.color_) {}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionColor(const VertexPositionColor& instance) noexcept
+		: position_(instance.position_)
+		, color_(instance.color_) {}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionColor& operator=(VertexPositionColor&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position_ = instance.position_;
+		color_ = instance.color_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 위치 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionColor& operator=(const VertexPositionColor& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position_ = instance.position_;
+		color_ = instance.color_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 정점의 바이트 보폭 값을 얻습니다.
+	 *
+	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
+	 */
+	static uint32_t GetStride()
+	{
+		return sizeof(VertexPositionColor);
+	}
+
+
+	/**
+	 * @brief 정점의 위치입니다.
+	 */
+	Vector3f position_;
+
+
+	/**
+	 * @brief 정점의 색상입니다.
+	 */
+	Vector4f color_;
+};
