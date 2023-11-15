07bfa5933b5c451535c2543334f281b910e8c09b
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-15 20:29:09 +0900

    힙메모리 손상 버그 수정 후 테스트 코드 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -19,12 +19,12 @@ int main(int argc, char* argv[])
 	GeometryShader2D* geometryShader = ResourceManager::Get().CreateResource<GeometryShader2D>("GeometryShader2D");
 	geometryShader->Initialize(shaderPath + "Geometry2D.vert", shaderPath + "Geometry2D.frag");
 
-	//TextureShader2D* textureShader = ResourceManager::Get().CreateResource<TextureShader2D>("TextureShader2D");
-	//textureShader->Initialize(shaderPath + "Texture2D.vert", shaderPath + "Texture2D.frag");
-	//std::string resourcePath;
-	//CommandLineArg::GetStringValue("resource", resourcePath);
-	//Texture2D* texture2d = ResourceManager::Get().CreateResource<Texture2D>("Texture");
-	//texture2d->Initialize(resourcePath + "awesomeface_4x4.astc");
+	TextureShader2D* textureShader = ResourceManager::Get().CreateResource<TextureShader2D>("TextureShader2D");
+	textureShader->Initialize(shaderPath + "Texture2D.vert", shaderPath + "Texture2D.frag");
+	std::string resourcePath;
+	CommandLineArg::GetStringValue("resource", resourcePath);
+	Texture2D* texture2d = ResourceManager::Get().CreateResource<Texture2D>("Texture");
+	texture2d->Initialize(resourcePath + "awesomeface_4x4.astc");
 	//texture2d->Initialize(resourcePath + "awesomeface.png");
 	
 	while (!glfwWindowShouldClose(window))
@@ -40,7 +40,7 @@ int main(int argc, char* argv[])
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
 		Matrix4x4f ortho = MathUtils::CreateOrtho(0.0f, 1000.0f, 800.0f, 0.0f, -1.0f, 1.0f);
-		geometryShader->DrawTriangle2D(ortho, Vector2f(0.0f, 0.0f), Vector2f(500.0f, 800.0f), Vector2f(700.0f, 400.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
+		textureShader->DrawTexture2D(ortho, texture2d, Vector2f(500.0f, 400.0f), 100.0f, 100.0f, 0.0f, 1.0f);
 
 		RenderManager::Get().EndFrame();
 	}
