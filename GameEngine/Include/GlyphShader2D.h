def6afb4ec0bbc2638a77dd0cda57c42ec23922f
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-18 09:20:12 +0900

    2D 텍스트를 그리는 기능 추가

---
+++ GameEngine/Include/GlyphShader2D.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <array>
+
+#include "Shader.h"
+#include "Vertex.h"
+
+class TTFont; // TTFont를 사용하기 위한 전방 선언입니다.
+
+
+/**
+ * @brief 글리프를 이용해서 텍스트를 렌더링하는 셰이더입니다.
+ * 
+ * @note
+ * - 2D 텍스트 렌더링 시 기준이 되는 좌표계는 윈도우 좌표계입니다.
+ */
+class GlyphShader2D : public Shader
+{
+public:
+	/**
+	 * @brief 2D 텍스트를 렌더링하는 셰이더의 디폴트 생성자입니다.
+	 *
+	 * @note 생성자 이외의 메서드에서 적절한 초기화를 수행해야 합니다.
+	 */
+	GlyphShader2D() = default;
+
+
+	/**
+	 * @brief 2D 텍스트를 렌더링하는 셰이더의 가상 소멸자입니다.
+	 *
+	 * @note 셰이더의 내부에서 할당된 요소를 해제하기 위해서는 반드시 Release를 호출해야 합니다.
+	 */
+	virtual ~GlyphShader2D();
+
+
+	/**
+	 * @brief 2D 텍스트를 렌더링하는 셰이더의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(GlyphShader2D);
+
+
+	/**
+	 * @brief GLSL 셰이더 소스 파일을 컴파일하고 셰이더 프로그램을 생성합니다.
+	 *
+	 * @param vsFile 버텍스 셰이더의 확장자(.vert)를 포함한 경로입니다.
+	 * @param fsFile 프레그먼트 셰이더의 확장자(.frag)를 포함한 경로입니다.
+	 * @param bCheckValid 셰이더 파일의 유효성 검사 여부입니다. 기본 값은 true입니다.
+	 */
+	virtual void Initialize(const std::string& vsPath, const std::string& fsPath, bool bCheckValid = true) override;
+
+
+	/**
+	 * @brief 셰이더의 내부 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 2D 텍스트를 화면에 그립니다.
+	 * 
+	 * @param ortho 직교 투영 행렬입니다.
+	 * @param font 폰트 리소스입니다.
+	 * @param text 렌더링할 텍스트입니다.
+	 * @param center 텍스트의 중심 좌표입니다.
+	 * @param color 텍스트의 RGBA 색상입니다.
+	 */
+	void DrawText2D(const Matrix4x4f& ortho, TTFont* font, const std::wstring& text, const Vector2f& center, const Vector4f& color);
+
+
+private:
+	/**
+	 * @brief 버텍스 버퍼를 업데이트합니다.
+	 */
+	void UpdateVertexBuffer();
+
+
+private:
+	/**
+	 * @brief 최대 문자열의 길이입니다.
+	 */
+	static const int32_t MAX_STRING_LEN = 1000;
+
+
+	/**
+	 * @brief 글리프 정점 목록의 최대 크기입니다.
+	 * 
+	 * @note
+	 * - 문자 하나 그리는 데 필요한 정점의 수 : 6개
+	 * - 만약, 최대 문자열의 길이가 1000이라면, 필요한 정점의 수 : 6 X 1000 = 6000개
+	 * - 즉, 이 글리프 셰이더가 지원하는 최대 문자열의 길이는 1000이다.
+	 */
+	static const int32_t MAX_VERTEX_SIZE = MAX_STRING_LEN * 6;
+
+
+	/**
+	 * @brief 정점 목록입니다.
+	 */
+	std::array<VertexPositionTexture, MAX_VERTEX_SIZE> vertices_;
+
+
+	/**
+	 * @brief 정점 버퍼의 오브젝트입니다.
+	 */
+	uint32_t vertexBufferObject_ = 0;
+
+
+	/**
+	 * @brief 정점 버퍼 목록의 오브젝트입니다.
+	 */
+	uint32_t vertexArrayObject_ = 0;
+};
---
+++ GameEngine/Source/GlyphShader2D.cpp
@@ -0,0 +1,124 @@
+#include "GlyphShader2D.h"
+
+#include "GLAssertionMacro.h"
+#include "TTFont.h"
+
+#include <glad/glad.h>
+
+GlyphShader2D::~GlyphShader2D()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void GlyphShader2D::Initialize(const std::string& vsPath, const std::string& fsPath, bool bCheckValid)
+{
+	ASSERT(!bIsInitialized_, "already initialize glyph shader 2d resource...");
+
+	Shader::Initialize(vsPath, fsPath, bCheckValid);
+
+	GL_ASSERT(glGenVertexArrays(1, &vertexArrayObject_), "failed to generate glyph vertex array...");
+	GL_ASSERT(glGenBuffers(1, &vertexBufferObject_), "failed to generate glyph vertex buffer...");
+
+	GL_ASSERT(glBindVertexArray(vertexArrayObject_), "failed to bind glyph vertex array...");
+	GL_ASSERT(glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject_), "failed to bind glyph vertex buffer...");
+	GL_ASSERT(glBufferData(GL_ARRAY_BUFFER, VertexPositionTexture::GetStride() * vertices_.size(), reinterpret_cast<const void*>(vertices_.data()), GL_DYNAMIC_DRAW),
+		"failed to create a new data store for a glyph buffer object...");
+
+	GL_ASSERT(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, VertexPositionTexture::GetStride(), (void*)(offsetof(VertexPositionTexture, position_))),
+		"failed to specify the location and data format of the array of generic vertex attributes at index...");
+	GL_ASSERT(glEnableVertexAttribArray(0), "failed to enable vertex attrib array...");
+
+	GL_ASSERT(glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, VertexPositionTexture::GetStride(), (void*)(offsetof(VertexPositionTexture, st_))),
+		"failed to specify the location and data format of the array of generic vertex attributes at index...");
+	GL_ASSERT(glEnableVertexAttribArray(1), "failed to enable vertex attrib array...");
+
+	GL_ASSERT(glBindVertexArray(0), "failed to unbind glyph vertex array...");
+}
+
+void GlyphShader2D::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+
+	Shader::Release();
+
+	GL_ASSERT(glDeleteBuffers(1, &vertexBufferObject_), "failed to delete glyph vertex buffer...");
+	GL_ASSERT(glDeleteVertexArrays(1, &vertexArrayObject_), "failed to delete glyph vertex array object...");
+}
+
+void GlyphShader2D::DrawText2D(const Matrix4x4f& ortho, TTFont* font, const std::wstring& text, const Vector2f& center, const Vector4f& color)
+{
+	ASSERT(text.length() <= MAX_STRING_LEN, L"overflow text length for draw : %d", text.length());
+	ASSERT(font != nullptr, "invalid font resource...");
+
+	float glyphAtlasSize = static_cast<float>(font->GetGlyphAtlasSize());
+
+	float textWidth = 0.0f;
+	float textHeight = 0.0f;
+	font->MeasureText(text, textWidth, textHeight);
+
+	Vector2f position(center.x - textWidth / 2.0f, center.y + textHeight / 2.0f);
+
+	int32_t vertexCount = 0;
+	for (const auto& unicode : text)
+	{
+		const Glyph& glyph = font->GetGlyph(static_cast<int32_t>(unicode));
+		
+		float unicodeWidth = static_cast<float>(glyph.position1.x - glyph.position0.x);
+		float unicodeHeight = static_cast<float>(glyph.position1.y - glyph.position0.y);
+
+		vertices_[vertexCount + 0].position_ = Vector3f(position.x + glyph.xoffset, position.y + glyph.yoffset, 0.0f);
+		vertices_[vertexCount + 0].st_ = Vector2f(static_cast<float>(glyph.position0.x) / glyphAtlasSize, static_cast<float>(glyph.position0.y) / glyphAtlasSize);
+		
+		vertices_[vertexCount + 1].position_ = Vector3f(position.x + glyph.xoffset, position.y + unicodeHeight + glyph.yoffset, 0.0f);
+		vertices_[vertexCount + 1].st_ = Vector2f(static_cast<float>(glyph.position0.x) / glyphAtlasSize, static_cast<float>(glyph.position1.y) / glyphAtlasSize);
+
+
+		vertices_[vertexCount + 2].position_ = Vector3f(position.x + glyph.xoffset + unicodeWidth, position.y + glyph.yoffset, 0.0f);
+		vertices_[vertexCount + 2].st_ = Vector2f(static_cast<float>(glyph.position1.x) / glyphAtlasSize, static_cast<float>(glyph.position0.y) / glyphAtlasSize);
+
+		vertices_[vertexCount + 3].position_ = Vector3f(position.x + glyph.xoffset + unicodeWidth, position.y + glyph.yoffset, 0.0f);
+		vertices_[vertexCount + 3].st_ = Vector2f(static_cast<float>(glyph.position1.x) / glyphAtlasSize, static_cast<float>(glyph.position0.y) / glyphAtlasSize);
+
+
+		vertices_[vertexCount + 4].position_ = Vector3f(position.x + glyph.xoffset, position.y + unicodeHeight + glyph.yoffset, 0.0f);
+		vertices_[vertexCount + 4].st_ = Vector2f(static_cast<float>(glyph.position0.x) / glyphAtlasSize, static_cast<float>(glyph.position1.y) / glyphAtlasSize);
+
+		vertices_[vertexCount + 5].position_ = Vector3f(position.x + glyph.xoffset + unicodeWidth, position.y + unicodeHeight + glyph.yoffset, 0.0f);
+		vertices_[vertexCount + 5].st_ = Vector2f(static_cast<float>(glyph.position1.x) / glyphAtlasSize, static_cast<float>(glyph.position1.y) / glyphAtlasSize);
+		
+		position.x += glyph.xadvance;
+		vertexCount += 6;
+	}
+
+	UpdateVertexBuffer();
+	
+	Shader::Bind();
+
+	GL_ASSERT(glActiveTexture(GL_TEXTURE0), "failed to active glyph texture atlas...");
+	GL_ASSERT(glBindTexture(GL_TEXTURE_2D, font->GetGlyphAtlasID()), "failed to bind glyph texture atlas...");
+
+	Shader::SetMatrix4x4fParameter("ortho", ortho);
+	Shader::SetVector4fParameter("glyphColor", color);
+
+	glBindVertexArray(vertexArrayObject_);
+	glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+	glBindVertexArray(0);
+
+	Shader::Unbind();
+}
+
+void GlyphShader2D::UpdateVertexBuffer()
+{
+	GL_ASSERT(glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject_), "failed to bind glyph vertex buffer...");
+
+	void* bufferPtr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	ASSERT(bufferPtr != nullptr, "failed to map the entire data store of a specified buffer object into the client's address space...");
+
+	std::memcpy(bufferPtr, reinterpret_cast<const void*>(vertices_.data()), VertexPositionTexture::GetStride() * vertices_.size());
+	GLboolean bSuccssed = glUnmapBuffer(GL_ARRAY_BUFFER);
+	ASSERT(bSuccssed, "failed to unmap the entire data store of a specified buffer object into the client's address space...");
+	GL_ASSERT(glBindBuffer(GL_ARRAY_BUFFER, 0), "failed to unbind 2d glyph vertex buffer...");
+}
