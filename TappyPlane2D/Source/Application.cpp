367fb6d8a406e6735c7d072eebb76772fc399eea
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-19 11:45:31 +0900

    스프라이트 애니메이션 테스트 코드 추가

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -26,9 +26,28 @@ int main(int argc, char* argv[])
 
 	TTFont* font = ResourceManager::Get().CreateResource<TTFont>("Font");
 	font->Initialize(resourcePath + "Font\\kenvector_future.ttf", 32, 127, 64.0f);
-	
+
+	std::array<Texture2D*, 5> planes = {
+		ResourceManager::Get().CreateResource<Texture2D>("Plane1"),
+		ResourceManager::Get().CreateResource<Texture2D>("Plane2"),
+		ResourceManager::Get().CreateResource<Texture2D>("Plane3"),
+		nullptr,
+		nullptr,
+	};
+
+	planes[0]->Initialize(resourcePath + "Texture\\planeBlue1.png");
+	planes[1]->Initialize(resourcePath + "Texture\\planeBlue2.png");
+	planes[2]->Initialize(resourcePath + "Texture\\planeBlue3.png");
+	planes[3] = planes[1];
+	planes[4] = planes[0];
+	float accumulateTime = 0.0f;
+	int32_t currentPlaneIndex = 0;
+
+	GameTimer timer;
+	timer.Reset();
 	while (!glfwWindowShouldClose(window))
 	{
+		timer.Tick();
 		glfwPollEvents();
 
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -44,8 +63,15 @@ int main(int argc, char* argv[])
 
 		RenderManager::Get().DrawHorizonScrollTexture2D(background, static_cast<float>(value));
 		RenderManager::Get().DrawHorizonScrollTexture2D(ground, Vector2f(500.0f, 770.0f), 1000.0f, 60.0f, value);
-		RenderManager::Get().DrawText2D(font, L"Hello, World!", Vector2f(500.0f, 400.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
 
+		accumulateTime += timer.GetDeltaSeconds();
+		if (accumulateTime >= 0.1f)
+		{
+			accumulateTime = 0.0f;
+			currentPlaneIndex = (currentPlaneIndex + 1) % (planes.size());
+		}
+		RenderManager::Get().DrawTexture2D(planes[currentPlaneIndex], Vector2f(500.0f, 400.0f), 88.0f, 73.0f, 0.0f);
+		
 		RenderManager::Get().EndFrame();
 	}
 
