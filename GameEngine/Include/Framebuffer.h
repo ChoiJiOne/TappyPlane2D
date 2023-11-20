214e1d3327c499d8a76de6395f3a0eaa2d496c86
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-20 16:16:24 +0900

    렌더링 가능하고 파이프라인이 참조 가능한 프레임 버퍼 추가

---
+++ GameEngine/Include/Framebuffer.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "IResource.h"
+
+
+/**
+ * @brief 파이프라인 참조 및 렌더링 가능한 프레임 버퍼입니다.
+ */
+class Framebuffer : public IResource
+{
+public:
+	/**
+	 * @brief 프레임 버퍼의 디폴트 생성자입니다.
+	 * 
+	 * @note 생성자 이외의 메서드에서 적절한 초기화를 수행해야 합니다.
+	 */
+	Framebuffer() = default;
+
+
+	/**
+	 * @brief 프레임 버퍼의 가상 소멸자입니다.
+	 */
+	virtual ~Framebuffer();
+
+
+	/**
+	 * @brief 프레임 버퍼의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Framebuffer);
+
+
+	/**
+	 * @brief 프레임 버퍼를 생성합니다.
+	 * 
+	 * @param bufferWidth 프레임 버퍼의 가로 크기입니다.
+	 * @param bufferHeight 프레임 버퍼의 세로 크기입니다.
+	 */
+	void Initialize(int32_t bufferWidth, int32_t bufferHeight);
+
+
+	/**
+	 * @brief 프레임 버퍼 내부의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 프레임 버퍼를 초기화합니다.
+	 * 
+	 * @param red 색상 버퍼의 R 값입니다.
+	 * @param green 색상 버퍼의 G 값입니다.
+	 * @param blue 색상 버퍼의 B 값입니다.
+	 * @param alpha 색상 버퍼의 A 값입니다.
+	 * @param depth 깊이 버퍼의 초기화 값입니다.
+	 * @param stencil 스텐실 버퍼의 초기화 값입니다.
+	 */
+	void Clear(float red, float green, float blue, float alpha, float depth = 1.0f, uint8_t stencil = 0);
+
+
+	/**
+	 * @brief 프레임 버퍼를 바인딩합니다.
+	 */
+	void Bind();
+
+
+	/**
+	 * @brief 프레임 버퍼의 바인딩을 해제합니다.
+	 */
+	void Unbind();
+
+
+	/**
+	 * @brief 프레임 버퍼의 텍스처를 활성화합니다.
+	 *
+	 * @param unit 활성화 할 텍스처 유닛입니다.
+	 */
+	void Active(uint32_t unit);
+
+
+private:
+	/**
+	 * @brief 바인딩 되었는지 확인합니다.
+	 */
+	bool bIsBind_ = false;
+
+
+	/**
+	 * @brief 프레임 버퍼 오브젝트의 ID입니다.
+	 */
+	uint32_t framebufferID_ = 0;
+
+
+	/**
+	 * @brief 컬러 버퍼 오브젝트의 ID입니다.
+	 */
+	uint32_t colorBufferID_ = 0;
+
+
+	/**
+	 * @brief 깊이 스텐실 버퍼 오브젝트의 ID입니다.
+	 */
+	uint32_t depthStencilBufferID_ = 0;
+};
---
+++ GameEngine/Source/Framebuffer.cpp
@@ -0,0 +1,90 @@
+#include "Framebuffer.h"
+
+#include "GLAssertionMacro.h"
+
+#include <glad/glad.h>
+
+Framebuffer::~Framebuffer()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Framebuffer::Initialize(int32_t bufferWidth, int32_t bufferHeight)
+{
+	ASSERT(!bIsInitialized_, "already initialize 2d texture resource...");
+	ASSERT((bufferWidth >= 0 && bufferHeight >= 0), "invalid frame buffer size : %d, %d", bufferWidth, bufferHeight);
+
+	GL_ASSERT(glGenFramebuffers(1, &framebufferID_), "failed to generate framebuffer object...");
+	GL_ASSERT(glBindFramebuffer(GL_FRAMEBUFFER, framebufferID_), "failed to bind frame buffer object...");
+
+	GL_ASSERT(glGenTextures(1, &colorBufferID_), "failed to generate color buffer object...");
+	GL_ASSERT(glBindTexture(GL_TEXTURE_2D, colorBufferID_), "failed to bind color buffer object...");
+	GL_ASSERT(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, bufferWidth, bufferHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr), "failed to allows elements of an image array to be read by shaders.");
+	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE), "failed to set texture object warp s...");
+	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE), "failed to set texture object warp t...");	
+	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR), "failed to set texture object min filter...");
+	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR), "failed to set texture object mag filter...");
+	GL_ASSERT(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBufferID_, 0), "");
+
+	GL_ASSERT(glGenRenderbuffers(1, &depthStencilBufferID_), "failed to generate depth stencil buffer object...");
+	GL_ASSERT(glBindRenderbuffer(GL_RENDERBUFFER, depthStencilBufferID_), "failed to bind depth stencil buffer object...");
+	GL_ASSERT(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, bufferWidth, bufferHeight), "failed to set depth stencil buffer size...");
+	GL_ASSERT(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, depthStencilBufferID_), "failed to set framebuffer depth stencil buffer...");
+
+	GLenum state = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	GL_ASSERT(state == GL_FRAMEBUFFER_COMPLETE, "not complete framebuffer state : %x...", static_cast<int32_t>(state));
+	
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	bIsInitialized_ = true;
+}
+
+void Framebuffer::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+
+	GL_ASSERT(glDeleteRenderbuffers(1, &depthStencilBufferID_), "failed to delete depth stencil buffer object...");
+	GL_ASSERT(glDeleteTextures(1, &colorBufferID_), "failed to delete color buffer object...");
+	GL_ASSERT(glDeleteFramebuffers(1, &framebufferID_), "failed to delete frame buffer object...");
+	
+	bIsInitialized_ = false;
+}
+
+void Framebuffer::Clear(float red, float green, float blue, float alpha, float depth, uint8_t stencil)
+{
+	if (!bIsBind_)
+	{
+		Bind();
+	}
+
+	glClearColor(red, green, blue, alpha);
+	glClearDepth(depth);
+	glClearStencil(stencil);
+	GL_ASSERT(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT), "failed to clear framebuffer...");
+}
+
+void Framebuffer::Bind()
+{
+	if (!bIsBind_)
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, framebufferID_);
+		bIsBind_ = true;
+	}
+}
+
+void Framebuffer::Unbind()
+{
+	if (bIsBind_)
+	{
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		bIsBind_ = false;
+	}
+}
+
+void Framebuffer::Active(uint32_t unit)
+{
+	GL_ASSERT(glActiveTexture(GL_TEXTURE0 + unit), "failed to active texture unit : %d", (GL_TEXTURE0 + unit));
+	GL_ASSERT(glBindTexture(GL_TEXTURE_2D, colorBufferID_), "failed to bind color buffer object...");
+}
