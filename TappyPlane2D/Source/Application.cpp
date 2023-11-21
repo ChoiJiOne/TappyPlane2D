a15b65e816759206b17d2bd25382bc90a47e0723
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-21 09:37:45 +0900

    ASTC 압축 텍스처 로딩 후 렌더링하는 테스트 코드 추가
    - 대부분 하드 코딩된 상태

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -18,6 +18,29 @@ int main(int argc, char* argv[])
 	std::string resourcePath;
 	CommandLineArg::GetStringValue("resource", resourcePath);
 
+	std::array<std::string, 14> astcTextures = {
+		"awesomeface4x4",
+		"awesomeface5x4",
+		"awesomeface5x5",
+		"awesomeface6x5",
+		"awesomeface6x6",
+		"awesomeface8x5",
+		"awesomeface8x6",
+		"awesomeface8x8",
+		"awesomeface10x5",
+		"awesomeface10x6",
+		"awesomeface10x8",
+		"awesomeface10x10",
+		"awesomeface12x10",
+		"awesomeface12x12",
+	};
+
+	for (const auto& astcTexture : astcTextures)
+	{
+		Texture2D* texture = ResourceManager::Get().CreateResource<Texture2D>(astcTexture);
+		texture->Initialize(StringUtils::PrintF("%sTest\\%s.astc", resourcePath.c_str(), astcTexture.c_str()));
+	}
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
@@ -29,6 +52,27 @@ int main(int argc, char* argv[])
 
 		RenderManager::Get().SetViewport(0, 0, 1000, 800);
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+		float x = 100.0f;
+		float y = 100.0f;
+		float width = 200.0f;
+		float height = 200.0f;
+
+		for (std::size_t index = 0; index < astcTextures.size(); ++index)
+		{
+			Texture2D* texture = ResourceManager::Get().GetResource<Texture2D>(astcTextures[index]);
+			RenderManager::Get().DrawTexture2D(texture, Vector2f(x, y), width, height, 0.0f);
+
+			if (index % 4 == 3)
+			{
+				x = 100.0f;
+				y += height;
+			}
+			else
+			{
+				x += width;
+			}
+		}
 		
 		RenderManager::Get().EndFrame();
 	}
