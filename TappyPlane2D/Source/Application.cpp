a94ec6b77789d63081fe4f7d7b6b7a49ec210cf8
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-23 17:43:04 +0900

    백그라운드와 그라운드 오브젝트 생성 및 업데이트/렌더링 수행
    - 본격적인 TappyPlane2D 개발을 위한 전체 초기화
    - 게임 루프에서 동작하도록 구현

--- TappyPlane2D/Source/Application.cpp
+++ TappyPlane2D/Source/Application.cpp
@@ -1,5 +1,8 @@
 #include "EngineManager.h"
 
+#include "Background.h"
+#include "Ground.h"
+
 int main(int argc, char* argv[])
 {
 	EngineManager::Get().Startup();
@@ -7,72 +10,28 @@ int main(int argc, char* argv[])
 	RenderManager::Get().SetAlphaBlend(true);
 	RenderManager::Get().SetDepthMode(false);
 
-	std::string resourcePath;
-	CommandLineArg::GetStringValue("resource", resourcePath);
+	GameTimer globalTimer;
+	globalTimer.Reset();
 
-	Texture2D* background = ResourceManager::Get().CreateResource<Texture2D>("Background");
-	background->Initialize(resourcePath + "Texture\\background.png");
+	Background* background = ObjectManager::Get().CreateGameObject<Background>("Background");
+	background->Initialize();
 
-	Texture2D* ground = ResourceManager::Get().CreateResource<Texture2D>("Ground");
-	ground->Initialize(resourcePath + "Texture\\groundGrass.png");
-
-	TTFont* font = ResourceManager::Get().CreateResource<TTFont>("Font");
-	font->Initialize(resourcePath + "Font\\kenvector_future.ttf", 32, 127, 64.0f);
-
-	std::array<Texture2D*, 5> planes = {
-		ResourceManager::Get().CreateResource<Texture2D>("Plane1"),
-		ResourceManager::Get().CreateResource<Texture2D>("Plane2"),
-		ResourceManager::Get().CreateResource<Texture2D>("Plane3"),
-		nullptr,
-		nullptr,
-	};
-
-	planes[0]->Initialize(resourcePath + "Texture\\planeBlue1.png");
-	planes[1]->Initialize(resourcePath + "Texture\\planeBlue2.png");
-	planes[2]->Initialize(resourcePath + "Texture\\planeBlue3.png");
-	planes[3] = planes[1];
-	planes[4] = planes[0];
-	float accumulateTime = 0.0f;
-	int32_t currentPlaneIndex = 0;
-
-	GameTimer timer;
-	timer.Reset();
-
+	Ground* ground = ObjectManager::Get().CreateGameObject<Ground>("Ground");
+	ground->Initialize();
+	
 	while (!InputManager::Get().ShouldCloseWindow())
 	{
-		timer.Tick();
+		globalTimer.Tick();
 		InputManager::Get().Tick();
 
-		RenderManager::Get().SetViewport(0, 0, 1000, 800);
-		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
-
-		float time = timer.GetTotalSeconds() / 10.0f;
-		float value = time - static_cast<float>(static_cast<int32_t>(time));
+		background->Update(globalTimer.GetDeltaSeconds());
+		ground->Update(globalTimer.GetDeltaSeconds());
 
 		RenderManager::Get().SetViewport(0, 0, 1000, 800);
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-		RenderManager::Get().DrawHorizonScrollTexture2D(background, static_cast<float>(value));
-		RenderManager::Get().DrawHorizonScrollTexture2D(ground, Vector2f(500.0f, 770.0f), 1000.0f, 60.0f, value);
-
-		accumulateTime += timer.GetDeltaSeconds();
-		if (accumulateTime >= 0.1f)
-		{
-			accumulateTime = 0.0f;
-			currentPlaneIndex = (currentPlaneIndex + 1) % (planes.size());
-		}
-		RenderManager::Get().DrawTexture2D(
-			planes[currentPlaneIndex],
-			Vector2f(500.0f, 400.0f),
-			88.0f,
-			73.0f,
-			0.0f
-		);
-		 
-		if (InputManager::Get().GetMouseButtonPressState(EMouseButton::BUTTON_LEFT) == EPressState::Held)
-		{
-			RenderManager::Get().PostEffectGaussianBlur(0.5f);
-		}
+		background->Render();
+		ground->Render();
 
 		RenderManager::Get().EndFrame();
 	}
