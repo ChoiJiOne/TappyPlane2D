d1df800330cd8ebb6d3f1e2396f75b870c1145c4
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-23 16:01:50 +0900

    게임 오브젝트 인터페이스 추가
    - 게임 내의 모든 오브젝트는 이 인터페이스를 상속받도록 구현할 예정

---
+++ GameEngine/Include/IGameObject.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "Macro.h"
+
+
+/**
+ * @brief 게임 내의 오브젝트 인터페이스입니다.
+ */
+class IGameObject
+{
+public:
+	/**
+	 * @brief 게임 오브젝트 인터페이스의 디폴트 생성자입니다.
+	 * 
+	 * @note 생성자 이외의 메서드에서 적절한 초기화를 수행해야 합니다.
+	 */
+	IGameObject() = default;
+
+
+	/**
+	 * @brief 게임 오브젝트 인터페이스의 가상 소멸자입니다.
+	 * 
+	 * @note 게임 오브젝트 내부에서 할당된 리소스를 해제하기 위해서는 Release 메서드를 호출해야 합니다.
+	 */
+	virtual ~IGameObject() {}
+
+
+	/**
+	 * @brief 게임 오브젝트 인터페이스의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(IGameObject);
+
+
+	/**
+	 * @brief 게임 오브젝트를 업데이트합니다.
+	 *
+	 * @param deltaSeconds 델타 시간 값입니다.
+	 * 
+	 * @note
+	 * - 게임 오브젝트 인터페이스를 상속받는 하위 클래스에서 반드시 구현해야 합니다.
+	 * - 델타 시간 값은 초단위입니다.
+	 */
+	virtual void Update(float deltaSeconds) = 0;
+
+
+	/**
+	 * @brief 게임 오브젝트를 화면에 그립니다.
+	 *
+	 * @note 게임 오브젝트 인터페이스를 상속받는 하위 클래스에서 반드시 구현해야 합니다.
+	 */
+	virtual void Render() = 0;
+
+
+	/**
+	 * @brief 게임 오브젝트 내의 리소스를 할당 해제합니다.
+	 *
+	 * @note 게임 오브젝트 인터페이스를 상속받는 하위 클래스에서 반드시 구현해야 합니다.
+	 */
+	virtual void Release() = 0;
+
+
+protected:
+	/**
+	 * @brief 게임 오브젝트가 초기화되었는지 확인합니다.
+	 */
+	bool bIsInitialized_ = false;
+};
