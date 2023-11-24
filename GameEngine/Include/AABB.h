95a6a6a03b3e6673f7bb13733fe994208fc7e700
CHOIJIONE\choij <choijione02@gmail.com>
2023-11-24 15:06:53 +0900

    게임 오브젝트의 AABB 추가
    - 충돌 처리 기능 구현된 상태

---
+++ GameEngine/Include/AABB.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "IShape.h"
+#include "Vector2.h"
+
+
+/**
+ * @brief 축 정렬 경계 상자(AABB) 형태입니다.
+ */
+class AABB : public IShape
+{
+public:
+	/**
+	 * @brief AABB의 생성자입니다.
+	 * 
+	 * @param minPosition 축 정렬 경계 상자의 최소 좌표입니다.
+	 * @param maxPosition 축 정렬 경계 상자의 최대 좌표입니다.
+	 */
+	AABB(const Vector2f& minPosition, const Vector2f& maxPosition);
+
+
+	/**
+	 * @brief AABB의 생성자입니다.
+	 * 
+	 * @param center 축 정렬 경계 상자의 중심 좌표입니다.
+	 * @param width 축 정렬 경계 상자의 가로 크기입니다.
+	 * @param height 축 정렬 경계 상자의 세로 크기입니다.
+	 */
+	AABB(const Vector2f& center, const float& width, const float& height);
+
+
+	/**
+	 * @brief AABB의 복사 생성자입니다.
+	 * 
+	 * @param instance 복사할 AABB 인스턴스입니다.
+	 */
+	AABB(AABB&& instance) noexcept
+		: minPosition_(instance.minPosition_)
+		, maxPosition_(instance.maxPosition_) {}
+
+
+	/**
+	 * @brief AABB의 복사 생성자입니다.
+	 * 
+	 * @param instance 복사할 AABB 인스턴스입니다.
+	 */
+	AABB(const AABB& instance) noexcept
+		: minPosition_(instance.minPosition_)
+		, maxPosition_(instance.maxPosition_) {}
+
+
+	/**
+	 * @brief AABB의 대입 연산자입니다.
+	 * 
+	 * @param instance 복사할 AABB 인스턴스입니다.
+	 * 
+	 * @return 대입한 AABB의 참조자를 반환합니다.
+	 */
+	AABB& operator=(AABB&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		minPosition_ = instance.minPosition_;
+		maxPosition_ = instance.maxPosition_;
+
+		return *this;
+	}
+	
+
+	/**
+	 * @brief AABB의 대입 연산자입니다.
+	 *
+	 * @param instance 복사할 AABB 인스턴스입니다.
+	 *
+	 * @return 대입한 AABB의 참조자를 반환합니다.
+	 */
+	AABB& operator=(const AABB& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		minPosition_ = instance.minPosition_;
+		maxPosition_ = instance.maxPosition_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief AABB의 타입을 반환합니다.
+	 * 
+	 * @return AABB의 타입을 반환합니다.
+	 */
+	virtual EType GetType() const override { return IShape::EType::AABB; }
+
+
+	/**
+	 * @brief AABB와 다른 오브젝트와의 충돌 여부를 검사합니다.
+	 * 
+	 * @param shape 충돌 검사를 수행할 오브젝트입니다.
+	 * 
+	 * @return 다른 오브젝트와 충돌한다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	virtual bool IsCollision(const IShape* shape) const override;
+
+
+private:
+	/**
+	 * @brief AABB 오브젝트와의 충돌 여부를 검사합니다.
+	 * 
+	 * @param shape 충돌 여부를 검사할 AABB 오브젝트입니다.
+	 * 
+	 * @return 다른 AABB 오브젝트와 충돌한다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsCollisionAABB(const IShape* shape) const;
+
+
+private:
+	/**
+	 * @brief 상자 영역의 최소값입니다.
+	 */
+	Vector2f minPosition_;
+
+
+	/**
+	 * @brief 상자 영역의 최대값입니다.
+	 */
+	Vector2f maxPosition_;
+};
---
+++ GameEngine/Source/AABB.cpp
@@ -0,0 +1,64 @@
+#include "AABB.h"
+
+#include "AssertionMacro.h"
+#include "MathUtils.h"
+
+AABB::AABB(const Vector2f& minPosition, const Vector2f& maxPosition)
+{
+	minPosition_ = Vector2f(
+		MathUtils::Min<float>(minPosition.x, maxPosition_.x),
+		MathUtils::Min<float>(minPosition.y, maxPosition_.y)
+	);
+
+	maxPosition_ = Vector2f(
+		MathUtils::Max<float>(minPosition.x, maxPosition_.x),
+		MathUtils::Max<float>(minPosition.y, maxPosition_.y)
+	);
+}
+
+AABB::AABB(const Vector2f& center, const float& width, const float& height)
+{
+	minPosition_ = center - Vector2f(width / 2.0f, height / 2.0f);
+	maxPosition_ = center + Vector2f(width / 2.0f, height / 2.0f);
+}
+
+bool AABB::IsCollision(const IShape* shape) const
+{
+	EType type = shape->GetType();
+	bool bIsCollision = false;
+
+	switch (type)
+	{
+	case IShape::EType::None:
+		bIsCollision = false;
+		break;
+
+	case IShape::EType::AABB:
+		bIsCollision = IsCollisionAABB(shape);
+		break;
+
+	default:
+		ASSERT(false, "undefined object shape type : %d", static_cast<int32_t>(type));
+	}
+	
+	return bIsCollision;
+}
+
+bool AABB::IsCollisionAABB(const IShape* shape) const
+{
+	ASSERT(shape->GetType() == EType::AABB, "must be of the AABB shape type...");
+
+	const AABB* aabb = reinterpret_cast<const AABB*>(shape);
+	
+	if (minPosition_.x > aabb->maxPosition_.x || maxPosition_.x < aabb->minPosition_.x)
+	{
+		return false;
+	}
+
+	if (minPosition_.y > aabb->maxPosition_.y || maxPosition_.y < aabb->minPosition_.y)
+	{
+		return false;
+	}
+	
+	return true;
+}
