51305c76da545b6844f5a864d4d05921a4913473
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-28 22:53:13 +0900

    플레이어가 획득 가능한 별 오브젝트 추가

---
+++ TappyPlane2D/Include/Star.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "AABB.h"
+#include "IGameObject.h"
+#include "Texture2D.h"
+#include "Vector2.h"
+
+
+/**
+ * @brief 플레이어가 획득 가능한 별 오브젝트입니다.
+ */
+class Star : public IGameObject
+{
+public:
+	/**
+	 * @brief 별 오브젝트의 디폴트 생성자입니다.
+	 */
+	Star() = default;
+
+
+	/**
+	 * @brief 별 오브젝트의 가상 소멸자입니다.
+	 * 
+	 * @note 별 오브젝트 내부에서 할당된 리소스를 해제하기 위해서는 Release 메서드를 호출해야 합니다.
+	 */
+	virtual ~Star();
+
+
+	/**
+	 * @brief 별 오브젝트의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Star);
+
+
+	/**
+	 * @brief 별 오브젝트를 초기화합니다.
+	 * 
+	 * @param center 별 오브젝트의 중심 위치입니다.
+	 */
+	void Initialize(const Vector2f& center);
+
+
+	 /**
+	  * @brief 별 오브젝트를 업데이트합니다.
+	  *
+	  * @param deltaSeconds 델타 시간 값입니다.
+	  */
+	virtual void Update(float deltaSeconds) override;
+
+
+	/**
+	 * @brief 별 오브젝트를 화면에 그립니다.
+	 */
+	virtual void Render() override;
+
+
+	/**
+	 * @brief 별 오브젝트 내의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 별 오브젝트의 충돌 경계 영역을 얻습니다.
+	 *
+	 * @return 별 오브젝트 충돌 경계 영역의 상수 참조자를 반환합니다.
+	 */
+	const AABB& GetCollisionBound() const { return collisionBound_; }
+
+
+private:
+	/**
+	 * @brief 별 오브젝트의 중심 위치입니다.
+	 */
+	Vector2f center_;
+
+
+	/**
+	 * @brief 별 오브젝트의 가로 크기입니다.
+	 */
+	float width_ = 0.0f;
+
+
+	/**
+	 * @brief 별 오브젝트의 세로 크기입니다.
+	 */
+	float height_ = 0.0f;
+
+
+	/**
+	 * @brief 별 오브젝트의 이동 속도입니다.
+	 */
+	float speed_ = 0.0f;
+
+
+	/**
+	 * @brief 별 오브젝트의 경계 영역입니다.
+	 */
+	AABB collisionBound_;
+
+
+	/**
+	 * @brief 별 오브젝트의 텍스처입니다.
+	 */
+	Texture2D* texture_ = nullptr;
+};
---
+++ TappyPlane2D/Source/Star.cpp
@@ -0,0 +1,47 @@
+#include "Star.h"
+
+#include "AssertionMacro.h"
+#include "RenderManager.h"
+#include "ResourceManager.h"
+#include "MathUtils.h"
+
+Star::~Star()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Star::Initialize(const Vector2f& center)
+{
+	ASSERT(!bIsInitialized_, "already initialize star game object...");
+
+	center_ = center;
+	width_ = 39.0f;
+	height_ = 38.0f;
+	speed_ = 200.0f;
+
+	texture_ = ResourceManager::Get().GetResource<Texture2D>("starGold");
+
+	collisionBound_.SetProperty(center_, width_, height_);
+
+	bIsInitialized_ = true;
+}
+
+void Star::Update(float deltaSeconds)
+{
+	center_.x -= speed_ * deltaSeconds;
+	collisionBound_.SetProperty(center_, width_, height_);
+}
+
+void Star::Render()
+{
+	RenderManager::Get().DrawTexture2D(texture_, center_, width_, height_, 0.0f);
+}
+
+void Star::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+	bIsInitialized_ = false;
+}
