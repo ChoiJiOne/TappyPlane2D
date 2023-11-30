a4c7d0b1830b13da7b96a641247aa9557c1eb6cc
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-11-30 09:06:32 +0900

    게임 씬 인터페이스 추가

---
+++ GameEngine/Include/IScene.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "Macro.h"
+
+
+/**
+ * @brief 게임 씬 인터페이스입니다.
+ * 
+ * @note 게임 내의 씬 오브젝트는 모두 이 인터페이스를 상속받아야합니다.
+ */
+class IScene
+{
+public:
+	/**
+	 * @brief 게임 씬의 디폴트 생성자입니다.
+	 */
+	IScene() = default;
+
+
+	/**
+	 * @brief 게임 씬의 가상 소멸자입니다.
+	 */
+	virtual ~IScene() {}
+
+
+	/**
+	 * @brief 게임 씬의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(IScene);
+
+
+	/**
+	 * @brief 게임 씬에 입장합니다.
+	 */
+	virtual void EnterScene() = 0;
+
+
+	/**
+	 * @brief 게임 씬에서 퇴장합니다.
+	 */
+	virtual void ExitScene() = 0;
+
+
+	/**
+	 * @brief 게임 씬의 한 프레임을 진행합니다.
+	 * 
+	 * @param deltaSeconds 델타 시간값입니다.
+	 */
+	virtual void TickScene(float deltaSeconds) = 0;
+
+
+	/**
+	 * @brief 현재 게임 씬의 다음 씬을 얻습니다.
+	 * 
+	 * @return 현재 게임 씬에 연결된 다음 씬의 포인터 값을 반환합니다.
+	 */
+	virtual IScene* GetNextScene() = 0;
+};
