7f0eb7e0eda7b783dcc8c41c4a6826929a6e6ba2
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-04 10:54:45 +0900

    게임 시작 씬에 입장했는지 확인하는 기능 추가

--- TappyPlane2D/Include/StartScene.h
+++ TappyPlane2D/Include/StartScene.h
@@ -43,6 +43,14 @@ public:
 
 
 	/**
+	 * @brief 게임 시작 씬에 입장했는지 확인합니다.
+	 *
+	 * @return 게임 시작 씬에 입장했다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	virtual bool IsEnterScene() override { return bIsEnterScene_; }
+
+
+	/**
 	 * @brief 게임 시작 씬의 한 프레임을 진행합니다.
 	 *
 	 * @param deltaSeconds 델타 시간값입니다.
