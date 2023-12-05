e1c5f31562e97ede070452982b25be48e1058660
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-05 11:15:18 +0900

    플레이 씬에 필요한 오브젝트 선언 추가

--- TappyPlane2D/Include/PlayScene.h
+++ TappyPlane2D/Include/PlayScene.h
@@ -5,12 +5,34 @@
 #include "IScene.h"
 #include "IGameObject.h"
 
+class Background;
+class Ground;
+class ObjectScheduler;
+class Plane;
+class ScoreBoard;
+class ClickTrigger;
+
 
 /**
  * @brief 게임의 플레이 씬입니다.
  */
 class PlayScene : public IScene
 {
+public:
+	/**
+	 * @brief 게임 플레이 씬의 상태입니다.
+	 */
+	enum class ESceneState : int32_t
+	{
+		Wait  = 0x00,
+		Enter = 0x01,
+		Play  = 0x02,
+		Pause = 0x03,
+		Crash = 0x04,
+		Exit  = 0x05,
+	};
+
+
 public:
 	/**
 	 * @brief 게임 플레이 씬의 디폴트 생성자입니다.
@@ -82,13 +104,61 @@ private:
 
 
 	/**
+	 * @brief 현재 게임 플레이 씬의 상태입니다.
+	 */
+	ESceneState currentSceneState_ = ESceneState::Wait;
+
+
+	/**
+	 * @brief 입장 상태가 되었을 때의 누적 시간값입니다.
+	 */
+	float enterAccumulateTime_ = 0.0f;
+
+
+	/**
 	 * @brief 현재 게임 플레이 씬에 연결된 다음 씬입니다.
 	 */
 	IScene* nextScene_ = nullptr;
 
 
 	/**
-	 * @brief 게임 플레이 씬의 오브젝트들입니다.
+	 * @brief 백그라운드 오브젝트입니다.
 	 */
-	std::array<IGameObject*, 5> objects_;
+	Background* background_ = nullptr;
+
+
+	/**
+	 * @brief 그라운드 오브젝트입니다.
+	 */
+	Ground* ground_ = nullptr;
+
+
+	/**
+	 * @brief 오브젝트 스캐줄러(Rock, Star)입니다.
+	 */
+	ObjectScheduler* scheduler_ = nullptr;
+
+
+	/**
+	 * @brief 비행기 오브젝트입니다.
+	 */
+	Plane* plane_ = nullptr;
+
+
+	/**
+	 * @brief 스코어 보드 오브젝트입니다.
+	 */
+	ScoreBoard* board_ = nullptr;
+
+
+	/**
+	 * @brief 게임이 중지되었을 때 계속할 지 확인하는 트리거입니다.
+	 */
+	ClickTrigger* continueTrigger_ = nullptr;
+
+
+	/**
+	 * @brief 게임을 다시 시작하는 트리거입니다.
+	 */
+	ClickTrigger* restartTrigger_ = nullptr;
 };
--- TappyPlane2D/Source/PlayScene.cpp
+++ TappyPlane2D/Source/PlayScene.cpp
@@ -1,10 +1,12 @@
 #include "PlayScene.h"
 
 #include "AssertionMacro.h"
+#include "InputManager.h"
 #include "ObjectManager.h"
 #include "RenderManager.h"
 
 #include "Background.h"
+#include "ClickTrigger.h"
 #include "Ground.h"
 #include "ObjectScheduler.h"
 #include "Plane.h"
@@ -19,66 +21,119 @@ PlayScene::~PlayScene()
 void PlayScene::EnterScene()
 {
 	bIsEnterScene_ = true;
+	currentSceneState_ = ESceneState::Enter;
+	enterAccumulateTime_ = 0.0f;
 
-	Background* background = ObjectManager::Get().GetGameObject<Background>("Background");
-	if (!background)
-	{
-		background = ObjectManager::Get().CreateGameObject<Background>("Background");
-		background->Initialize();
-	}
-
-	Ground* ground = ObjectManager::Get().GetGameObject<Ground>("Ground");
-	if (!ground)
-	{
-		ground = ObjectManager::Get().CreateGameObject<Ground>("Ground");
-		ground->Initialize(Ground::EType::Grass);
-	}
-
-	ObjectScheduler* scheduler = ObjectManager::Get().GetGameObject<ObjectScheduler>("Scheduler");
-	if (!scheduler)
-	{
-		scheduler = ObjectManager::Get().CreateGameObject<ObjectScheduler>("Scheduler");
-		scheduler->Initialize(Rock::EType::Plain, 1100.0f);
-	}
-
-	Plane* plane = ObjectManager::Get().GetGameObject<Plane>("Plane");
-	if (!plane)
-	{
-		plane = ObjectManager::Get().CreateGameObject<Plane>("Plane");
-		plane->Initialize(Plane::EColor::Yellow);
-	}
-
-	ScoreBoard* board = ObjectManager::Get().GetGameObject<ScoreBoard>("Board");
-	if(!board)
-	{
-		board = ObjectManager::Get().CreateGameObject<ScoreBoard>("Board");
-		board->Initialize(Vector2f(500.0f, 100.0f), Vector4f(0.3f, 0.3f, 0.3f, 1.0f));
-	}
-
-	objects_ = {
-		background,
-		ground,
-		scheduler,
-		plane,
-		board,
+	std::function<void()> pauseTrigger = [&]() 
+	{ 
+		if (currentSceneState_ == ESceneState::Play && plane_->GetState() == Plane::EState::Flight)
+		{
+			currentSceneState_ = ESceneState::Pause;
+		}
 	};
+	InputManager::Get().BindWindowEventAction("Minimize", EWindowEvent::EnterMinimize, pauseTrigger);
+	InputManager::Get().BindWindowEventAction("LostFocus", EWindowEvent::LostFocus, pauseTrigger);
+	InputManager::Get().BindWindowEventAction("Move", EWindowEvent::Move, pauseTrigger);
+	
+    background_ = ObjectManager::Get().GetGameObject<Background>("Background");
+
+	std::array<Ground::EType, 5> groundTypes = {
+		Ground::EType::Dirt,
+		Ground::EType::Grass,
+		Ground::EType::Ice,
+		Ground::EType::Rock,
+		Ground::EType::Snow,
+	};
+	ground_ = ObjectManager::Get().GetGameObject<Ground>("Ground");
+	if (!ground_)
+	{
+		ground_ = ObjectManager::Get().CreateGameObject<Ground>("Ground");
+	}
+	ground_->Initialize(groundTypes.at(MathUtils::GenerateRandomInt(0, static_cast<int32_t>(groundTypes.size()) - 1)));
+
+	std::array<Rock::EType, 4> rockTypes = {
+		Rock::EType::Plain,
+		Rock::EType::Grass,
+		Rock::EType::Ice,
+		Rock::EType::Snow,
+	};
+	scheduler_ = ObjectManager::Get().GetGameObject<ObjectScheduler>("Scheduler");
+	if (!scheduler_)
+	{
+		scheduler_ = ObjectManager::Get().CreateGameObject<ObjectScheduler>("Scheduler");
+	}
+	scheduler_->Initialize(rockTypes.at(MathUtils::GenerateRandomInt(0, static_cast<int32_t>(rockTypes.size()) - 1)), 1100.0f);
+	
+	std::array<Plane::EColor, 4> planeColors = {
+		Plane::EColor::Blue,
+		Plane::EColor::Green,
+		Plane::EColor::Red,
+		Plane::EColor::Yellow,
+	};
+	plane_ = ObjectManager::Get().GetGameObject<Plane>("Plane");
+	if (!plane_)
+	{
+		plane_ = ObjectManager::Get().CreateGameObject<Plane>("Plane");
+	}
+	plane_->Initialize(planeColors.at(MathUtils::GenerateRandomInt(0, static_cast<int32_t>(planeColors.size()) - 1)));
+	
+	board_ = ObjectManager::Get().GetGameObject<ScoreBoard>("Board");
+	if (!board_)
+	{
+		board_ = ObjectManager::Get().CreateGameObject<ScoreBoard>("Board");
+	}
+	board_->Initialize(Vector2f(500.0f, 100.0f), Vector4f(0.3f, 0.3f, 0.3f, 1.0f));
+
+	continueTrigger_ = ObjectManager::Get().GetGameObject<ClickTrigger>("ContinueTrigger");
+	if (!continueTrigger_)
+	{
+		continueTrigger_ = ObjectManager::Get().CreateGameObject<ClickTrigger>("ContinueTrigger");
+	}
+	continueTrigger_->Initialize(
+		L"PRESS MOUSE BUTTON",
+		Vector2f(500.0f, 400.0f),
+		Vector4f(1.0f, 0.0f, 0.0f, 1.0f),
+		[&]() 
+		{ 
+			if (currentSceneState_ == ESceneState::Pause)
+			{
+				currentSceneState_ = ESceneState::Play;
+			}
+		}
+	);
 }
 
 void PlayScene::ExitScene()
 {
 	bIsEnterScene_ = false;
+	currentSceneState_ = ESceneState::Wait;
 }
 
 void PlayScene::TickScene(float deltaSeconds)
 {
+	if (currentSceneState_ == ESceneState::Enter)
+	{
+		enterAccumulateTime_ += deltaSeconds;
+		if (enterAccumulateTime_ >= 1.0f)
+		{
+			currentSceneState_ = ESceneState::Play;
+		}
+	}
+
 	RenderManager& renderManager = RenderManager::Get();
 	renderManager.SetViewport(0, 0, 1000, 800);
 	renderManager.BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-	for (auto& object : objects_)
+	//for (auto& object : objects_)
+	//{
+	//	object->Update(deltaSeconds);
+	//	object->Render();
+	//}
+
+	if (currentSceneState_ == ESceneState::Enter)
 	{
-		object->Update(deltaSeconds);
-		object->Render();
+		float bias = MathUtils::Clamp<float>(enterAccumulateTime_, 0.0f, 1.0f);
+		renderManager.PostEffectFadeEffect(bias);
 	}
 
 	renderManager.EndFrame();
